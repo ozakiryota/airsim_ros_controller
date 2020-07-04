b4ec7abcac06b57af41a951a0924124fbbb8ed68
ozakiryota <kb22056@gmail.com>
2020-07-04 17:13:34 +0900

    first commit

---
+++ src/takeoff.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <airsim_ros_pkgs/Takeoff.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "takeoff");
+	ros::NodeHandle nh;
+
+	ros::ServiceClient client = nh.serviceClient<airsim_ros_pkgs::Takeoff>("/airsim_node/takeoff");
+
+	airsim_ros_pkgs::Takeoff srv;
+
+	if(client.call(srv)){
+		std::cout << "takeoff: true" << std::endl;
+		return 0;
+	}
+	else{
+		std::cout << "takeoff: false" << std::endl;
+		return 1;
+	}
+}
